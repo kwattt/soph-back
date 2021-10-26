654708db1eecfff942378d0286523a33d93e86bc
Jorge <jglez1616@gmail.com>
2021-10-25 22:44:08 -0500

    added missing messages router

--- src/routes/api/messages/index.ts
+++ src/routes/api/messages/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import welcome from './welcome';
 import commands from './commands';
+import oracle from './oracle';
 
 const router = Router();
 router.use(welcome)
+router.use(oracle)
 router.use(commands)
 
 export default router
